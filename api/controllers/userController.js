ec9e0eaf8a16280c6a22efb69c1533e030fc534b
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-21 21:24:53 +0200

    Setup user routes and controller

---
+++ api/controllers/userController.js
@@ -0,0 +1,7 @@
+exports.registerUSer = (req, res) => {
+  res.send('Register route');
+};
+
+exports.loginUser = (req, res) => {
+  res.send('Login route');
+};
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.post('/', userController.registerUSer);
+
+router.post('/login', userController.loginUser);
+
+module.exports = router;
