5b71defefaf690d389c9ead306c8b97e313fc9ca
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-21 23:02:51 +0200

    Added register and login logic

--- api/config/db.js
+++ api/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`.yellow.underline);
+    console.log(`MongoDB connected: ${conn.connection.host}`.blue.underline);
   } catch (err) {
     console.log(`Error : ${err.message}`.red.underline.bold);
     process.exit(1);
