e74e7ee0f5f8de45d69933faf3d76aff02d0d4e0
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-21 21:55:37 +0200

    Added error middleware

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,17 @@
-exports.registerUSer = (req, res) => {
+exports.registerUser = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.includes('@')) {
+    res.status(422);
+    throw new Error('Please enter a valid email');
+  }
+
+  const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (!password || password.trim().length < 6 || !format.test(password)) {
+    res.status(422);
+    throw new Error('Please enter a valid password');
+  }
+
   res.send('Register route');
 };
 
--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
   res.status(200).json({ message: 'Welcome!' });
@@ -10,4 +14,6 @@ app.get('/', (req, res, next) => {
 
 app.use('/api/users', require('./routes/userRoutes'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.use(errorHandler);
+
+app.listen(PORT);
