49567ec1dec06835a21009562d06f31f9d9bb3f6
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-23 23:40:53 +0200

    Initialised frontend via create react app

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -65,7 +65,11 @@ exports.loginUser = asyncHandler(async (req, res) => {
 });
 
 exports.getMe = asyncHandler(async (req, res) => {
-  res.send('me');
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 const generateToken = (id) => {
